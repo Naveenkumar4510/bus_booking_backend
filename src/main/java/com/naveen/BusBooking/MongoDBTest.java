10587b8c372ecbd7ea2828a242c8052eedb321e6
Naveen Kumar A P <92946607+Naveenkumar4510@users.noreply.github.com>
2024-06-20 17:51:17 +0530

    Update MongoDBTest.java

--- src/main/java/com/naveen/BusBooking/MongoDBTest.java
+++ src/main/java/com/naveen/BusBooking/MongoDBTest.java
@@ -1,1 +1,19 @@
 
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDBTest {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://naveen:12345@cluster0.arybrtd.mongodb.net/?retryWrites=true&w=majority&ssl=true";
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase database = mongoClient.getDatabase("bus-db");
+            System.out.println("Connected to the database successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
